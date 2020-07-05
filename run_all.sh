3efb4850980c098ad37d346cd4dde67d4da3a9b2
raphael <rapguit@gmail.com>
2020-07-05 20:46:08 -0300

    update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -4,9 +4,9 @@ nohup python main.py -e 500 -dp none -sc StandardScaler -runs 10 -lr 0.001 -f 10
 nohup python main.py -e 500 -dp none -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset happy > out_test_ss_happy.txt ;
 nohup python main.py -e 500 -dp none -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset kaggle_bkp > out_test_ss_kaggle_bkp.txt;
 
-nohup python main.py -e 500 -dp none -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset teddy > out_test_ss_cdp_teddy.txt ;
-nohup python main.py -e 500 -dp none -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset happy > out_test_ss_cpd_happy.txt ;
-nohup python main.py -e 500 -dp none -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset kaggle_bkp > out_test_ss_cdp_kaggle_bkp.txt;
+nohup python main.py -e 500 -dp ErrorBasedDropoutZero -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset teddy > out_test_ss_zero_teddy.txt ;
+nohup python main.py -e 500 -dp ErrorBasedDropoutZero -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset happy > out_test_ss_zero_happy.txt ;
+nohup python main.py -e 500 -dp ErrorBasedDropoutZero -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset kaggle_bkp > out_test_ss_zero_kaggle_bkp.txt;
 
 nohup python main.py -xgbr -e 500 -sc StandardScaler -lr 0.001 -f 10 -dataset teddy > out_test_ss_xgbr_teddy.txt;
 nohup python main.py -xgbr -e 500 -sc StandardScaler -lr 0.001 -f 10 -dataset happy > out_test_ss_xgbr_happy.txt;
