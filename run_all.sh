a3c815cac3827b0804bc123cdb9c9e6d0e500c14
raphael <rapguit@gmail.com>
2020-07-14 17:41:55 -0300

    add ErrorBasedDropoutIR approach on run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -8,6 +8,10 @@ nohup python main.py -e 500 -dp ErrorBasedDropoutZero -sc StandardScaler -runs 1
 nohup python main.py -e 500 -dp ErrorBasedDropoutZero -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset happy > out_test_ss_zero_happy.txt ;
 nohup python main.py -e 500 -dp ErrorBasedDropoutZero -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset kaggle_bkp > out_test_ss_zero_kaggle_bkp.txt;
 
+nohup python main.py -e 500 -dp ErrorBasedDropoutIR -sc StandardScaler -runs 10 -lr 0.001 -f 15 -dataset teddy > out_test_ss_ir_teddy.txt ;
+nohup python main.py -e 500 -dp ErrorBasedDropoutIR -sc StandardScaler -runs 10 -lr 0.001 -f 15 -dataset happy > out_test_ss_ir_happy.txt ;
+nohup python main.py -e 500 -dp ErrorBasedDropoutIR -sc StandardScaler -runs 10 -lr 0.001 -f 15 -dataset kaggle_bkp > out_test_ss_ir_kaggle_bkp.txt;
+
 nohup python main.py -xgbr -e 500 -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset teddy > out_test_ss_xgbr_teddy.txt;
 nohup python main.py -xgbr -e 500 -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset happy > out_test_ss_xgbr_happy.txt;
 nohup python main.py -xgbr -e 500 -sc StandardScaler -runs 10 -lr 0.001 -f 10 -dataset kaggle_bkp  > out_test_ss_xgbr_kaggle.txt;
