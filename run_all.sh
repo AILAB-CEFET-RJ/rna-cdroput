12e35ee4b04dc4b418b1581c1fd4af83b546040d
raphael <rapguit@gmail.com>
2020-09-11 22:36:15 -0300

    update sh file

--- run_all.sh
+++ run_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -cut -subs 120000 -dp ErrorBasedDropoutIR -sc StandardScaler -dataset sdss -hl1 790 -hl2 158 -noes > out_test_ss_sdss.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -cut -subs 120000 -dp ErrorBasedDropoutIR -sc StandardScaler -dataset sdss -hl1 791 -hl2 158 -noes > out_test_ss_sdss.txt;
 
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val B -dp ErrorBasedDropoutIR -sc StandardScaler -dataset teddy -hl1 790 -hl2 158 -noes > out_test_ss_teddy_B.txt;
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val C -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset teddy -hl1 790 -hl2 158 -noes > out_test_ss_teddy_C.txt;
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val D -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset teddy -hl1 790 -hl2 158 -noes > out_test_ss_teddy_D.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val B -dp ErrorBasedDropoutIR -sc StandardScaler -dataset teddy -hl1 791 -hl2 158 -noes > out_test_ss_teddy_B.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val C -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset teddy -hl1 791 -hl2 158 -noes > out_test_ss_teddy_C.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val D -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset teddy -hl1 791 -hl2 158 -noes > out_test_ss_teddy_D.txt;
 
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val B -dp ErrorBasedDropoutIR -sc StandardScaler -dataset happy -hl1 790 -hl2 158 -noes > out_test_ss_happy_B.txt;
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val C -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset happy -hl1 790 -hl2 158 -noes > out_test_ss_happy_C.txt;
-nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val D -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset happy -hl1 790 -hl2 158 -noes > out_test_ss_happy_D.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val B -dp ErrorBasedDropoutIR -sc StandardScaler -dataset happy -hl1 791 -hl2 158 -noes > out_test_ss_happy_B.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val C -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset happy -hl1 791 -hl2 158 -noes > out_test_ss_happy_C.txt;
+nohup python main.py -noes -e 3000 -runs 10 -lr 0.001 -f 15 -coin_val D -mo -dp ErrorBasedDropoutIR -sc StandardScaler -dataset happy -hl1 791 -hl2 158 -noes > out_test_ss_happy_D.txt;
 
