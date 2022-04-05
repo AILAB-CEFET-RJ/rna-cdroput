192ab7abbdeeeb706e81d92e48535103b381f701
Raphael Fialho <raphael.fialho@zoop.com.br>
2022-04-04 21:40:46 -0300

    bug fix for clean datasets script

--- dataset_clean.py
+++ dataset_clean.py
@@ -5,10 +5,10 @@ import pandas as pd
 def parser():
     parse = argparse.ArgumentParser(description='ANN Experiments. Script for dataset cleaning.')
     parse.add_argument('-dataset', metavar='DS', help='Dataset file to use.')
-    parser.add_argument('-rn', action='store_true', help='Remove negative entries.')
-    parser.add_argument('-u25', action='store_true', help='Cut >= 25 in u band.')
-    parser.add_argument('-e1', action='store_true', help='Cut >= 1.0 in all band errors.')
-    parser.add_argument('-rf', action='store_true', help='Cut clean flag data = 0.')
+    parse.add_argument('-rn', action='store_true', help='Remove negative entries.')
+    parse.add_argument('-u25', action='store_true', help='Cut >= 25 in u band.')
+    parse.add_argument('-e1', action='store_true', help='Cut >= 1.0 in all band errors.')
+    parse.add_argument('-rf', action='store_true', help='Cut clean flag data = 0.')
 
     return parse
 
@@ -26,31 +26,43 @@ def filter_negative_data(dataframe):
 
 
 def cut_all_val_errs(df, val):
+    orig_size = df.shape[0]
+
     for b in 'ugriz':
         df = df[df[f"{b}"] <= val]
+
+    clean_size = df.shape[0]
+    print(f"Errors values cut off: {orig_size - clean_size}.")
 
     return df
 
 
 def cut_val_band(df, band, val):
-    return df[df[band] <= val]
+    orig_size = df.shape[0]
+
+    df = df[df[band] <= val]
+
+    clean_size = df.shape[0]
+    print(f"Attr in {band} band cut: {orig_size - clean_size}.")
+
+    return df
 
 
 def cut_flag(df, attr, value):
-    chunk_size = df[df[attr] == value].shape[0]
-    print(f"{chunk_size} values cut off.")
+    orig_size = df.shape[0]
 
-    return df[df[attr] != value]
+    df = df[df[attr] != value]
+
+    clean_size = df.shape[0]
+    print(f"Flag values cut off: {orig_size - clean_size}.")
+
+    return df
 
 
 def clean_data(dataset_name, rm_negatives, cut_u_25, cut_1_errs, cut_clean_flag):
-    data = pd.read_csv(dataset_name, comment='#',
-                       delim_whitespace=True,
-                       names=[
-                           'ID', 'u', 'g', 'r', 'i', 'z',
-                           'uErr', 'gErr', 'rErr', 'iErr', 'zErr',
-                           'redshift', 'redshiftErr'
-                       ])
+    data = pd.read_csv(dataset_name, comment='#')
+
+    data_orig_size = data.shape[0]
 
     if rm_negatives:
         data = filter_negative_data(data)
@@ -64,11 +76,15 @@ def clean_data(dataset_name, rm_negatives, cut_u_25, cut_1_errs, cut_clean_flag)
     if cut_clean_flag:
         data = cut_flag(data, 'clean', 0)
 
-    data.to_csv(f"{dataset_name}_clean", index=False)
+    data_clean_size = data.shape[0]
+    print(f"Total data removed: {data_orig_size - data_clean_size}.")
+
+    name, ext = dataset_name.split('.')
+    data.to_csv(f"{name}_clean.{ext}", index=False)
 
 
 if __name__ == '__main__':
     parser = parser()
     args = parser.parse_args()
 
-    clean_data(*args)
+    clean_data(args.dataset, args.rn, args.u25, args.e1, args.rf)
