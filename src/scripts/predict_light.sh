3142cd0e2b40e945713f328376b156ee4a67f573
cassiofb-dev <cassiofb.souza@gmail.com>
2022-07-26 23:57:17 -0300

    bugfix sh

--- src/scripts/predict_light.sh
+++ src/scripts/predict_light.sh
@@ -33,11 +33,11 @@ python -m src.pipeline.dataset_split -dataset teddy_data_ir_1_x_1_experrs.csv -p
 
 python -m src.pipeline.dataset_scaling -datafiles teddy_data_ir_1_x_1_experrs_train.csv teddy_data_ir_1_x_1_experrs_val.csv teddy_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
 
-python -m src.training -n teddy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset teddy_data_ir_1_x_1_experrs_train_scaled.csv -valset teddy_data_ir_1_x_1_experrs_val_scaled.csv
+python -m src.training -n teddy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 2 -trainset teddy_data_ir_1_x_1_experrs_train_scaled.csv -valset teddy_data_ir_1_x_1_experrs_val_scaled.csv
 
-BEST_MODEL=$(python -m src.modules.best_model_selector -n teddy_training_ir_1_x_1)
+BEST_MODELS=$(python -m src.modules.best_model_selector -n teddy_training_ir_1_x_1 -e 10)
 
-python -m src.predict -model $BEST_MODEL -testset teddy_data_ir_1_x_1_experrs_test.csv -dp y
+eval "python -m src.predict -models ${BEST_MODELS} -testset teddy_data_ir_1_x_1_experrs_test.csv -dp y"
 
 ### HAPPY ###
 python -m src.strategies.ir_1_x_1 -dataset happy_data.csv
@@ -46,11 +46,11 @@ python -m src.pipeline.dataset_split -dataset happy_data_ir_1_x_1_experrs.csv -p
 
 python -m src.pipeline.dataset_scaling -datafiles happy_data_ir_1_x_1_experrs_train.csv happy_data_ir_1_x_1_experrs_val.csv happy_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
 
-python -m src.training -n happy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset happy_data_ir_1_x_1_experrs_train_scaled.csv -valset happy_data_ir_1_x_1_experrs_val_scaled.csv
+python -m src.training -n happy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 2 -trainset happy_data_ir_1_x_1_experrs_train_scaled.csv -valset happy_data_ir_1_x_1_experrs_val_scaled.csv
 
-BEST_MODEL=$(python -m src.modules.best_model_selector -n happy_training_ir_1_x_1)
+BEST_MODELS=$(python -m src.modules.best_model_selector -n happy_training_ir_1_x_1 -e 10)
 
-python -m src.predict -model $BEST_MODEL -testset happy_data_ir_1_x_1_experrs_test.csv -dp y
+eval "python -m src.predict -models ${BEST_MODELS} -testset happy_data_ir_1_x_1_experrs_test.csv -dp y"
 
 ### GENERATE ERROR RESULTS ###
 python -m src.report.error_results
