160f0e22f881c9fa1b1e794fe6d9796104326fc9
cassiofb-dev <cassiofb.souza@gmail.com>
2022-07-26 22:48:34 -0300

    multiple models

--- src/predict.py
+++ src/predict.py
@@ -12,7 +12,7 @@ from src.modules.regularization import ErrorBasedInvertedDropoutV2
 def parser():
     parse = argparse.ArgumentParser(description='ANN Experiments')
     parse.add_argument('-n', metavar='NAME', help='Experiment name. This name is included in output files.')
-    parse.add_argument('-model', metavar='MODEL', help='Model filename.')
+    parse.add_argument('-models', nargs='+', metavar='MODELS', help='Model filenames.')
     parse.add_argument('-dp', metavar='DROPOUT', help='Dropout class to use.')
     parse.add_argument('-testset', metavar='TESTSET', help='Test dataset file.')
     parse.add_argument('-bs', metavar='BATCH', type=int, default=32, help='Batch size.')
@@ -35,7 +35,7 @@ def custom_layer_register():
 def load_model(file):
     with tf.keras.utils.custom_object_scope(custom_layer_register()):
         m = tf.keras.models.load_model(file)
-        utils.rna_cdropout_print(f">>>  {file} Loaded !")
+        utils.rna_cdropout_print(f"{file} Loaded !")
 
         return m
 
@@ -47,7 +47,9 @@ def serialize_results(real, preds, mfile):
 
     file_name = mfile.split('/')[-1].split('.hdf5')[0]
     dump_file = f"real_x_pred_{file_name}.csv"
-    dump_dir = f"./output/preds/"
+    epochs = mfile.split('/')[2]
+    run = mfile.split('/')[3]
+    dump_dir = f"./output/preds/{epochs}/{run}/"
     if not os.path.exists(dump_dir):
         os.makedirs(dump_dir)
     filepath = os.path.join(dump_dir, dump_file)
@@ -57,30 +59,31 @@ def serialize_results(real, preds, mfile):
     utils.rna_cdropout_print(f"Result[{dump_file}] dumped!")
 
 
-def predict(model_file, testset, batch_size, dropout):
-    model = load_model(model_file)
-    utils.rna_cdropout_print(f"Using batch size: {batch_size}")
-    test_df = pd.read_csv(f"./src/data/{testset}", comment='#')
-    utils.rna_cdropout_print(f"Train set loaded! Shape = {test_df.shape}")
+def predict(model_files, testset, batch_size, dropout):
+    for model_file in model_files:
+        model = load_model(model_file)
+        utils.rna_cdropout_print(f"Using batch size: {batch_size}")
+        test_df = pd.read_csv(f"./src/data/{testset}", comment='#')
+        utils.rna_cdropout_print(f"Train set loaded! Shape = {test_df.shape}")
 
-    ugriz = list('ugriz')
-    errors = list(map(lambda b: f"err_{b}", ugriz))
-    exp_errors = list(map(lambda eb: f"{eb}_exp", errors))
-    target = ['redshift']
+        ugriz = list('ugriz')
+        errors = list(map(lambda b: f"err_{b}", ugriz))
+        exp_errors = list(map(lambda eb: f"{eb}_exp", errors))
+        target = ['redshift']
 
-    features = ugriz + errors
-    if dropout:
-        features = features + exp_errors
+        features = ugriz + errors
+        if dropout:
+            features = features + exp_errors
 
-    x_test = test_df[features]
-    y_test = test_df[target]
+        x_test = test_df[features]
+        y_test = test_df[target]
 
-    outputs = model.predict(x_test, batch_size=batch_size)
-    serialize_results(y_test, outputs, model_file)
+        outputs = model.predict(x_test, batch_size=batch_size)
+        serialize_results(y_test, outputs, model_file)
 
 
 if __name__ == '__main__':
     parser = parser()
     args = parser.parse_args()
 
-    predict(args.model, args.testset, args.bs, args.dp)
+    predict(args.models, args.testset, args.bs, args.dp)
