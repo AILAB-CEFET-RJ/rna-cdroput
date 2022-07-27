adb1afb69a7ddaa0473461028ec8e6d571be5876
cassiofb-dev <cassiofb.souza@gmail.com>
2022-07-26 22:50:35 -0300

    ir manual

--- src/scripts/predict_light.sh
+++ src/scripts/predict_light.sh
@@ -14,7 +14,7 @@ do
 
             eval "python -m src.pipeline.dataset_scaling -datafiles ${dataset}_data_${regressor}_${strategy}_experrs_train.csv ${dataset}_data_${regressor}_${strategy}_experrs_val.csv ${dataset}_data_${regressor}_${strategy}_experrs_test.csv -scaler StandardScaler"
 
-            eval "python -m src.training -n ${dataset}_training_${regressor}_${strategy} -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset ${dataset}_data_${regressor}_${strategy}_experrs_train_scaled.csv -valset ${dataset}_data_${regressor}_${strategy}_experrs_val_scaled.csv"
+            eval "python -m src.training -n ${dataset}_training_${regressor}_${strategy} -e 10 -dp ErrorBasedInvertedRandomDropout -runs 2 -trainset ${dataset}_data_${regressor}_${strategy}_experrs_train_scaled.csv -valset ${dataset}_data_${regressor}_${strategy}_experrs_val_scaled.csv"
 
             BEST_MODELS=$(python -m src.modules.best_model_selector -n ${dataset}_training_${regressor}_${strategy} -e 10)
 
@@ -22,6 +22,35 @@ do
         done
     done
 done
+
+### ISOTONIC REGRESSOR ###
+# ISOTONIC REGRESSOR DOES NOT SUPPORT MULTIPLE INPUT/OUTPUT!
+
+### TEDDY ###
+python -m src.strategies.ir_1_x_1 -dataset teddy_data.csv
+
+python -m src.pipeline.dataset_split -dataset teddy_data_ir_1_x_1_experrs.csv -p 60:20:20
+
+python -m src.pipeline.dataset_scaling -datafiles teddy_data_ir_1_x_1_experrs_train.csv teddy_data_ir_1_x_1_experrs_val.csv teddy_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
+
+python -m src.training -n teddy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset teddy_data_ir_1_x_1_experrs_train_scaled.csv -valset teddy_data_ir_1_x_1_experrs_val_scaled.csv
+
+BEST_MODEL=$(python -m src.modules.best_model_selector -n teddy_training_ir_1_x_1)
+
+python -m src.predict -model $BEST_MODEL -testset teddy_data_ir_1_x_1_experrs_test.csv -dp y
+
+### HAPPY ###
+python -m src.strategies.ir_1_x_1 -dataset happy_data.csv
+
+python -m src.pipeline.dataset_split -dataset happy_data_ir_1_x_1_experrs.csv -p 60:20:20
+
+python -m src.pipeline.dataset_scaling -datafiles happy_data_ir_1_x_1_experrs_train.csv happy_data_ir_1_x_1_experrs_val.csv happy_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
+
+python -m src.training -n happy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset happy_data_ir_1_x_1_experrs_train_scaled.csv -valset happy_data_ir_1_x_1_experrs_val_scaled.csv
+
+BEST_MODEL=$(python -m src.modules.best_model_selector -n happy_training_ir_1_x_1)
+
+python -m src.predict -model $BEST_MODEL -testset happy_data_ir_1_x_1_experrs_test.csv -dp y
 
 ### GENERATE ERROR RESULTS ###
 python -m src.report.error_results
