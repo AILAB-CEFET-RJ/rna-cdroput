6a2ce009a360c97703a6038f50a02fc5ca18f6cd
cassiofb-dev <cassiofb.souza@gmail.com>
2022-07-26 23:59:15 -0300

    ir script update

--- src/scripts/strategies/ir_1_x_1.sh
+++ src/scripts/strategies/ir_1_x_1.sh
@@ -5,11 +5,11 @@ python -m src.pipeline.dataset_split -dataset teddy_data_ir_1_x_1_experrs.csv -p
 
 python -m src.pipeline.dataset_scaling -datafiles teddy_data_ir_1_x_1_experrs_train.csv teddy_data_ir_1_x_1_experrs_val.csv teddy_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
 
-python -m src.training -n teddy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset teddy_data_ir_1_x_1_experrs_train_scaled.csv -valset teddy_data_ir_1_x_1_experrs_val_scaled.csv
+python -m src.training -n teddy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 2 -trainset teddy_data_ir_1_x_1_experrs_train_scaled.csv -valset teddy_data_ir_1_x_1_experrs_val_scaled.csv
 
-BEST_MODEL=$(python -m src.modules.best_model_selector -n teddy_training_ir_1_x_1)
+BEST_MODELS=$(python -m src.modules.best_model_selector -n teddy_training_ir_1_x_1 -e 10)
 
-python -m src.predict -model $BEST_MODEL -testset teddy_data_ir_1_x_1_experrs_test.csv -dp y
+eval "python -m src.predict -models ${BEST_MODELS} -testset teddy_data_ir_1_x_1_experrs_test.csv -dp y"
 
 ### HAPPY ###
 python -m src.strategies.ir_1_x_1 -dataset happy_data.csv
@@ -18,11 +18,11 @@ python -m src.pipeline.dataset_split -dataset happy_data_ir_1_x_1_experrs.csv -p
 
 python -m src.pipeline.dataset_scaling -datafiles happy_data_ir_1_x_1_experrs_train.csv happy_data_ir_1_x_1_experrs_val.csv happy_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
 
-python -m src.training -n happy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset happy_data_ir_1_x_1_experrs_train_scaled.csv -valset happy_data_ir_1_x_1_experrs_val_scaled.csv
+python -m src.training -n happy_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 2 -trainset happy_data_ir_1_x_1_experrs_train_scaled.csv -valset happy_data_ir_1_x_1_experrs_val_scaled.csv
 
-BEST_MODEL=$(python -m src.modules.best_model_selector -n happy_training_ir_1_x_1)
+BEST_MODELS=$(python -m src.modules.best_model_selector -n happy_training_ir_1_x_1 -e 10)
 
-python -m src.predict -model $BEST_MODEL -testset happy_data_ir_1_x_1_experrs_test.csv -dp y
+eval "python -m src.predict -models ${BEST_MODELS} -testset happy_data_ir_1_x_1_experrs_test.csv -dp y"
 
 ### SDSS ###
 python -m src.strategies.ir_1_x_1 -dataset sdss_data.csv
@@ -31,8 +31,8 @@ python -m src.pipeline.dataset_split -dataset sdss_data_ir_1_x_1_experrs.csv -p 
 
 python -m src.pipeline.dataset_scaling -datafiles sdss_data_ir_1_x_1_experrs_train.csv sdss_data_ir_1_x_1_experrs_val.csv sdss_data_ir_1_x_1_experrs_test.csv -scaler StandardScaler
 
-python -m src.training -n sdss_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 1 -trainset sdss_data_ir_1_x_1_experrs_train_scaled.csv -valset sdss_data_ir_1_x_1_experrs_val_scaled.csv
+python -m src.training -n sdss_training_ir_1_x_1 -e 10 -dp ErrorBasedInvertedRandomDropout -runs 2 -trainset sdss_data_ir_1_x_1_experrs_train_scaled.csv -valset sdss_data_ir_1_x_1_experrs_val_scaled.csv
 
-BEST_MODEL=$(python -m src.modules.best_model_selector -n sdss_training_ir_1_x_1)
+BEST_MODELS=$(python -m src.modules.best_model_selector -n sdss_training_ir_1_x_1 -e 10)
 
-python -m src.predict -model $BEST_MODEL -testset sdss_data_ir_1_x_1_experrs_test.csv -dp y
+eval "python -m src.predict -models ${BEST_MODELS} -testset sdss_data_ir_1_x_1_experrs_test.csv -dp y"
