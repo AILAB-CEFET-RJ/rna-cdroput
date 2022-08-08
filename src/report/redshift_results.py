c0da9c0695bcb512285235bc0f77379a4df31b4d
cassiofb-dev <cassiofb.souza@gmail.com>
2022-08-08 03:54:48 -0300

    redshift results latex table

---
+++ src/report/redshift_results.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def init_redshift_results():
+    results_path = "output/scores.csv"
+
+    results_df = pd.read_csv(results_path)
+
+    results_df.sort_values("MSE_MEAN", inplace=True)
+
+    results_df.set_index("Name", inplace=True)
+
+    results_df.index.name = "model"
+
+    results_df = results_df[["MSE_MEAN", "RMSE_MEAN", "MAD_MEAN", "R2_MEAN"]]
+
+    results_df.index = results_df.index.map(lambda model: model.replace("_training", ""))
+
+    results_df.to_latex(
+        buf="src/report/tables/redshift_results" + ".tex",
+        label="tab:redshift_results",
+        caption="Predição de Redshifts com os Modelos de Regressão",
+    )
+
+if __name__ == "__main__":
+    init_redshift_results()
+
