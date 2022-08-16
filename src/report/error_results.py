cad8f5cef4ce0c1aeb157a84eb9245610d319b71
cassiofb-dev <cassiofb.souza@gmail.com>
2022-08-16 08:38:20 -0300

    padronize table

--- src/report/error_results.py
+++ src/report/error_results.py
@@ -5,11 +5,23 @@ if __name__ == "__main__":
 
     results_df.drop(columns=["timestamp"], inplace=True)
 
-    results_df.set_index("mse", inplace=True)
-
-    results_df.sort_index(inplace=True)
-
     results_df.drop_duplicates(inplace=True)
+
+    results_df.rename(columns={
+        "mse": "MSE",
+        "mae": "MAE",
+        "r2": "R2",
+    }, inplace=True)
+
+    results_df["MODEL"] = results_df["dataset"] + "_" + results_df["regressor"] + "_" +  results_df["estratégia"]
+
+    results_df["MODEL"].apply(lambda name: name.replace("_data", ""))
+
+    results_df.drop(columns=["dataset", "regressor", "estratégia"], inplace=True)
+
+    results_df.sort_values(by=["MSE"], inplace=True)
+
+    results_df.set_index("MODEL", inplace=True)
 
     results_df.to_latex(
         buf="./src/report/tables/errors_results.tex",
