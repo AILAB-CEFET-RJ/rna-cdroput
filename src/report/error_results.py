96cecb20bcb1e8e8ad1ebef4101d32d674dfdef8
cassiofb-dev <cassiofb.souza@gmail.com>
2022-08-16 08:43:42 -0300

    remove inplace

--- src/report/error_results.py
+++ src/report/error_results.py
@@ -3,25 +3,25 @@ import pandas as pd
 if __name__ == "__main__":
     results_df = pd.read_csv("./src/report/tables/errors_results.csv")
 
-    results_df.drop(columns=["timestamp"], inplace=True)
+    results_df = results_df.drop(columns=["timestamp"])
 
-    results_df.drop_duplicates(inplace=True)
+    results_df = results_df.drop_duplicates()
 
-    results_df.rename(columns={
+    results_df = results_df.rename(columns={
         "mse": "MSE",
         "mae": "MAE",
         "r2": "R2",
-    }, inplace=True)
+    })
 
     results_df["MODEL"] = results_df["dataset"] + "_" + results_df["regressor"] + "_" +  results_df["estratégia"]
 
-    results_df["MODEL"].apply(lambda name: name.replace("_data", ""))
+    results_df["MODEL"] = results_df["MODEL"].apply(lambda name: name.replace("_data", ""))
 
-    results_df.drop(columns=["dataset", "regressor", "estratégia"], inplace=True)
+    results_df = results_df.drop(columns=["dataset", "regressor", "estratégia"])
 
-    results_df.sort_values(by=["MSE"], inplace=True)
+    results_df = results_df.sort_values(by=["MSE"])
 
-    results_df.set_index("MODEL", inplace=True)
+    results_df = results_df.set_index("MODEL")
 
     results_df.to_latex(
         buf="./src/report/tables/errors_results.tex",
